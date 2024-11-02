1737a436d4f2b9b672aa5fa0c169e6e3e1f9513e
kennethadwa <147363175+kennethadwa@users.noreply.github.com>
2024-11-03 03:39:37 +0800

    added table on products

--- admin/view_products.php
+++ admin/view_products.php
@@ -1,54 +1,32 @@
 <?php
 session_start();
 if (!isset($_SESSION['account_type']) || $_SESSION['account_type'] != 0) {
-  header("Location: ../login-register.php");
-  exit();
+    header("Location: ../login-register.php");
+    exit();
 }
 
 include('../connection.php');
 
-// Fetch packages data with needed items from the database
-$query = "SELECT p.package_id, p.package_name, p.price, pp.product_id, pr.product_name 
-          FROM packages p 
-          LEFT JOIN package_products pp ON p.package_id = pp.package_id 
-          LEFT JOIN products pr ON pp.product_id = pr.product_id";
-$result = $conn->query($query);
-
-$packages = [];
-if ($result && $result->num_rows > 0) {
-  while ($row = $result->fetch_assoc()) {
-    $package_name = $row['package_name'];
-    // Create a new package entry if it doesn't exist
-    if (!isset($packages[$package_name])) {
-      $packages[$package_name] = [
-        'id' => $row['package_id'], // Store package ID for editing
-        'price' => '₱' . number_format($row['price'], 2), // Format the price as currency
-        'needed_items' => [] // Initialize needed items
-      ];
-    }
-    // Add the product name to the needed items array
-    if ($row['product_name']) {
-      $packages[$package_name]['needed_items'][] = $row['product_name'];
-    }
-  }
-} else {
-  $packages = []; // No packages found
-}
+// Fetch products from the database, joining with categories for the category name
+$query = "SELECT product_id, products.product_name, products.image, products.description, 
+                 products.price, products.quantity, categories.category_name 
+          FROM products 
+          LEFT JOIN categories ON products.category_id = categories.category_id";
+$result = mysqli_query($conn, $query);
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>View Packages</title>
-  <link href="vendor/jquery-nice-select/css/nice-select.css" rel="stylesheet">
-  <link rel="stylesheet" href="vendor/nouislider/nouislider.min.css">
-  <!-- Style CSS -->
-  <link href="css/style.css" rel="stylesheet">
-  <style>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>View Packages</title>
+    <link href="vendor/jquery-nice-select/css/nice-select.css" rel="stylesheet">
+    <link rel="stylesheet" href="vendor/nouislider/nouislider.min.css">
+    <link href="css/style.css" rel="stylesheet">
+    <style>
     body {
       background-color: #17153B;
     }
@@ -127,120 +105,112 @@ if ($result && $result->num_rows > 0) {
 </head>
 
 <body>
+    <div id="main-wrapper">
+        <?php include('nav-header.php'); ?>
+        <?php include('header.php'); ?>
+        <?php include('sidebar.php'); ?>
 
-  <!-- Main wrapper Start -->
-  <div id="main-wrapper">
-    <!-- Nav Header Start -->
-    <?php include('nav-header.php'); ?>
-    <!-- Nav Header End -->
+        <div class="content-body">
+            <div class="container-fluid">
+                <div class="row invoice-card-row">
+                    <div class="col-12">
+                        <div class="search-container d-flex justify-content-center">
+                            <input type="text" id="search-input" placeholder="Search products..." onkeyup="filterPackages()">
+                        </div>
 
-    <!-- Header Start -->
-    <?php include('header.php'); ?>
-    <!-- Header End -->
+                        <div class="category-navbar">
+                            <a href="#" data-category="" onclick="filterByCategory(event)">All</a>
+                            <a href="#" data-category="Motorcycle Parts" onclick="filterByCategory(event)">Motorcycle Parts</a>
+                            <a href="#" data-category="Accessories" onclick="filterByCategory(event)">Accessories</a>
+                            <a href="#" data-category="Tires & Wheels" onclick="filterByCategory(event)">Tires & Wheels</a>
+                            <a href="#" data-category="Oils & Lubricants" onclick="filterByCategory(event)">Oils & Lubricants</a>
+                            <a href="#" data-category="Battery" onclick="filterByCategory(event)">Battery</a>
+                            <a href="#" data-category="Electrical Components" onclick="filterByCategory(event)">Electrical Components</a>
+                            <a href="#" data-category="Suspension & Brakes" onclick="filterByCategory(event)">Suspension & Brakes</a>
+                            <a href="#" data-category="Maintenance Services" onclick="filterByCategory(event)">Maintenance Services</a>
+                            <a href="#" data-category="Performance Upgrades" onclick="filterByCategory(event)">Performance Upgrades</a>
+                            <a href="#" data-category="Custom Builds & Modifications" onclick="filterByCategory(event)">Custom Builds &   Modifications</a>
+                        </div>
 
-    <!-- Sidebar Start -->
-    <?php include('sidebar.php'); ?>
-    <!-- Sidebar End -->
+                        <!-- Products Table -->
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th style="background: transparent; color: white; text-align: center;">Image</th>
+                                    <th style="background: transparent; color: white; text-align: center;">Product</th>
+                                    <th style="background: transparent; color: white; text-align: center;">Description</th>
+                                    <th style="background: transparent; color: white; text-align: center;">Category</th>
+                                    <th style="background: transparent; color: white; text-align: center;">Price</th>
+                                    <th style="background: transparent; color: white; text-align: center;">Quantity</th>
+                                    <th style="background: transparent; color: white; text-align: center;">View Details</th>
+                                </tr>
+                            </thead>
+                            <tbody id="package-list" style="background: transparent;">
+                                <?php while ($row = mysqli_fetch_assoc($result)) : ?>
+                                    <tr class="pcard" data-category="<?php echo $row['category_name']; ?>">
+                                        <td style="background: transparent;">
+                                          <div class="d-flex justify-content-center" style="width: 100%; height: 100%;">
+                                          <img src="<?php echo $row['image']; ?>" alt="<?php echo $row['product_name']; ?>" width="50">
+                                        </div>
+                                        </td>
+                                        <td class="title" style="background: transparent; color: white; text-align: center;"><?php echo $row['product_name']; ?></td>
+                                        <td style="background: transparent; color: white; text-align: center;"><?php echo $row['description']; ?></td>
+                                        <td style="background: transparent; color: white; text-align: center;"><?php echo $row['category_name']; ?></td>
+                                        <td style="background: transparent; color: white; text-align: center;"><?php echo $row['price']; ?></td>
+                                        <td style="background: transparent; color: white; text-align: center;"><?php echo $row['quantity']; ?></td>
+                                        <td style="background: transparent; color: white; text-align: center;">
+                                          <a href="edit_product.php?product_id=<?php echo $row['product_id']; ?>" style="padding: 10px 20px; background: green; color: white; border-radius: 10px;">Edit</a>
+                                          &nbsp;
+                                          &nbsp;
+                                          <a href="delete_product.php?product_id=<?php echo $row['product_id']; ?>" style="padding: 10px; background: red; color: white; border-radius: 10px;" onclick="return confirmDelete()">Delete</a>
+                                        </td>
+                                    </tr>
+                                <?php endwhile; ?>
+                            </tbody>
+                        </table>
 
-    <!-- Content Body Start -->
-    <div class="content-body">
-      <div class="container-fluid">
-        <div class="row invoice-card-row">
-          <div class="col-12">
-            <div class="search-container d-flex justify-content-center">
-              <input type="text" id="search-input" placeholder="Search products..." onkeyup="filterPackages()">
+                    </div>
+                </div>
             </div>
-
-            <!-- Category Navbar -->
-            <div class="category-navbar">
-              <a href="#" data-category="Motorcycle Parts" onclick="filterByCategory(event)">Motorcycle Parts</a>
-              <a href="#" data-category="Accessories" onclick="filterByCategory(event)">Accessories</a>
-              <a href="#" data-category="Tires & Wheels" onclick="filterByCategory(event)">Tires & Wheels</a>
-              <a href="#" data-category="Oils & Lubricants" onclick="filterByCategory(event)">Oils & Lubricants</a>
-              <a href="#" data-category="Battery" onclick="filterByCategory(event)">Battery</a>
-              <a href="#" data-category="Electrical Components" onclick="filterByCategory(event)">Electrical Components</a>
-              <a href="#" data-category="Suspension & Brakes" onclick="filterByCategory(event)">Suspension & Brakes</a>
-              <a href="#" data-category="Maintenance Services" onclick="filterByCategory(event)">Maintenance Services</a>
-              <a href="#" data-category="Performance Upgrades" onclick="filterByCategory(event)">Performance Upgrades</a>
-              <a href="#" data-category="Custom Builds & Modifications" onclick="filterByCategory(event)">Custom Builds & Modifications</a>
-            </div>
-
-            <div class="card mb-4" style="box-shadow: none; background: transparent;">
-              <div class="card-body" id="package-list">
-                <?php include('./cards/product_ajax.php'); ?>
-              </div>
-            </div>
-          </div>
         </div>
-      </div>
     </div>
-    <!-- Content Body End -->
 
-  </div>
-  <!-- Main wrapper end -->
+    <!-- Scripts -->
+    <script src="vendor/global/global.min.js"></script>
+    <script src="vendor/chart.js/Chart.bundle.min.js"></script>
+    <script src="vendor/jquery-nice-select/js/jquery.nice-select.min.js"></script>
+    <script src="https://kit.fontawesome.com/b931534883.js" crossorigin="anonymous"></script>
+    <script src="vendor/apexchart/apexchart.js"></script>
+    <script src="vendor/nouislider/nouislider.min.js"></script>
+    <script src="vendor/wnumb/wNumb.js"></script>
+    <script src="js/dashboard/dashboard-1.js"></script>
+    <script src="js/custom.min.js"></script>
+    <script src="js/dlabnav-init.js"></script>
+    <script src="js/demo.js"></script>
+    <script src="js/styleSwitcher.js"></script>
 
-  <!-- Scripts -->
-  <script src="vendor/global/global.min.js"></script>
-  <script src="vendor/chart.js/Chart.bundle.min.js"></script>
-  <script src="vendor/jquery-nice-select/js/jquery.nice-select.min.js"></script>
-  <script src="https://kit.fontawesome.com/b931534883.js" crossorigin="anonymous"></script>
-
-  <script src="vendor/apexchart/apexchart.js"></script>
-  <script src="vendor/nouislider/nouislider.min.js"></script>
-  <script src="vendor/wnumb/wNumb.js"></script>
-
-  <script src="js/dashboard/dashboard-1.js"></script>
-
-  <script src="js/custom.min.js"></script>
-  <script src="js/dlabnav-init.js"></script>
-  <script src="js/demo.js"></script>
-  <script src="js/styleSwitcher.js"></script>
-
-  <script>
-
-function filterByCategory(event) {
+    <script>
+        function filterByCategory(event) {
   const category = event.target.getAttribute('data-category');
   const packageList = document.getElementById('package-list');
   const cards = packageList.getElementsByClassName('pcard');
 
-  // Show all cards if no category is selected
-  if (category === "") {
-    for (let i = 0; i < cards.length; i++) {
-      cards[i].style.display = '';
-    }
-    return;
-  }
-
-  // Filter cards based on category
+  // Show all cards if the "All" category is selected
   for (let i = 0; i < cards.length; i++) {
-    const packageCategory = cards[i].getAttribute('data-category'); // Assume you add a data-category attribute to your package cards
-    if (packageCategory === category) {
+    if (category === "" || cards[i].getAttribute('data-category') === category) {
       cards[i].style.display = '';
     } else {
       cards[i].style.display = 'none';
     }
   }
 }
+    </script>
 
-    function filterPackages() {
-      const input = document.getElementById('search-input');
-      const filter = input.value.toLowerCase();
-      const packageList = document.getElementById('package-list');
-      const cards = packageList.getElementsByClassName('pcard');
-
-      for (let i = 0; i < cards.length; i++) {
-        const packageName = cards[i].querySelector('.title').textContent.toLowerCase();
-        if (packageName.includes(filter)) {
-          cards[i].style.display = '';
-        } else {
-          cards[i].style.display = 'none';
-        }
-      }
-    }
-  </script>
-
-  
-
+    <script>
+function confirmDelete() {
+    return confirm("Are you sure you want to delete this package?");
+}
+</script>
 </body>
 
 </html>
